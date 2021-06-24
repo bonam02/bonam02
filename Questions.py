e8120f0a31b75440592e7459bdaf9032434f44a6
Sbonam <sbonam@Sbonams-MacBook-Pro.local>
2021-06-24 12:54:22 +0530

    basic questions

---
+++ Questions.py
@@ -0,0 +1,39 @@
+
+list vs tuple
+
+dict, defaultDict, OrderedDict
+
+module vs package
+
+lambda
+
+namespaces : Local namespace
+             Global namespace
+            Built-in namespace
+
+how tuples can be used as keys in dictionaries (why only tuples - Because tuples are hashable and lists are not)
+
+Ref : https://stackoverflow.com/questions/1938614/in-what-case-would-i-use-a-tuple-as-a-dictionary-key/1938638
+
+pickling and unpickling
+
+***Decorators
+
+Difference between generators and iterators
+
+"//" operator - floor division
+
+Switch is implemented in python 3.10
+
+Ex: def f(x):
+    match x:
+        case 'a':
+            return 1
+        case 'b':
+            return 2
+
+Ref : https://stackoverflow.com/questions/60208/replacements-for-switch-statement-in-python
+
+What is GIL : Global Interpreter Lock
+
+new Throwable.printstacktrace() in java = traceback.print_stack() in python
